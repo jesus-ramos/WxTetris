dc3282a4b4902681ff683d2e526d722e18588b4b
Jesus Ramos <jesus-ramos@live.com>
2011-11-30 00:47:49 -0500

    easier to read now that the lines are aligned

--- Board.hpp
+++ Board.hpp
@@ -7,26 +7,26 @@
 
 #define TIMER_INTERVAL 300
 
-const wxColour colors[] =
+const wxColour colors[] = 
 {
-    wxColour(0, 0, 0), wxColour(204, 102, 102),
+    wxColour(0, 0, 0),       wxColour(204, 102, 102),
     wxColour(102, 204, 102), wxColour(102, 102, 204),
     wxColour(204, 204, 102), wxColour(204, 102, 204),
     wxColour(102, 204, 204), wxColour(218, 170, 0)
 };
 
-const wxColour light[] =
+const wxColour light[] = 
 {
-    wxColour(0, 0, 0), wxColour(248, 159, 171),
+    wxColour(0, 0, 0),       wxColour(248, 159, 171),
     wxColour(121, 252, 121), wxColour(121, 121, 252),
     wxColour(252, 252, 121), wxColour(252, 121, 252),
     wxColour(121, 252, 252), wxColour(252, 198, 0)
 };
 
-const wxColour dark[] =
+const wxColour dark[] = 
 {
-    wxColour(0, 0, 0), wxColour(128, 59, 59),
-    wxColour(59, 128, 59), wxColour(59, 59, 128),
+    wxColour(0, 0, 0),      wxColour(128, 59, 59),
+    wxColour(59, 128, 59),  wxColour(59, 59, 128),
     wxColour(128, 128, 59), wxColour(128, 59, 128),
     wxColour(59, 128, 128), wxColour(128, 98, 0)
 };
