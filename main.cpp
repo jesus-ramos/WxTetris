f9e309e17600ea234aa2ca673d72d72cf079d3bd
Jesus Ramos <jesus-ramos@live.com>
2012-01-11 10:16:20 -0500

    Fixing commit that got reverted accidentally

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include "main.hpp"
 #include "TetrisGame.hpp"
 
+#ifdef __WXMAC__
+#include <ApplicationServices/ApplicationServices.h>
+#endif /* __WXMAC__ */
+
 IMPLEMENT_APP(TetrisApp);
 
 bool TetrisApp::OnInit()
 {
+#ifdef __WXMAC__
+    ProcessSerialNumber psn;
+    GetCurrentProcess(&psn);
+    TransformProcessType(&psn, kProcessTransformToForegroundApplication);
+#endif /* __WXMAC__ */
     TetrisGame* tetrisGame = new TetrisGame(wxT("WxTetris"));
     tetrisGame->Centre();
     tetrisGame->Show(true);
